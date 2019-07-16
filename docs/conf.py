55bec91bbaa753dc19f421538e67ae10b8db61f9
triskadecaepyon <dcltechnology@gmail.com>
2019-07-15 23:29:27 -0500

    adding in fix round 2

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,10 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+
+# ReadtheDocs-specific items
 autodoc_mock_imports = ['pyworkout']
+master_doc = 'index'
 
 # -- Project information -----------------------------------------------------
 
@@ -49,6 +52,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
+
 html_static_path = ['_static']
 
 # html_theme = "sphinx_rtd_theme"
